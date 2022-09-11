bae575703584968aa126327652daf055696557c0
杨赛雅 <10715064+yang-saiya@user.noreply.gitee.com>
2022-09-11 17:04:32 +0800

    1

---
+++ src/main/java/com/intrest/IntrestApplication.java
@@ -0,0 +1,13 @@
+package com.intrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntrestApplication.class, args);
+    }
+
+}
