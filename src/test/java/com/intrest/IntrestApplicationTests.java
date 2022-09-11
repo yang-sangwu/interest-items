22c4c4b541133249b664b2e265024d32248c5999
杨赛雅 <10715064+yang-saiya@user.noreply.gitee.com>
2022-09-11 17:11:24 +0800

    2

---
+++ src/test/java/com/intrest/IntrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
