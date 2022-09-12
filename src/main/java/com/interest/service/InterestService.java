62ca8c854221739649469fbc73db2f39c617e057
杨赛雅 <10715064+yang-saiya@user.noreply.gitee.com>
2022-09-12 16:24:44 +0800

    4

--- src/main/java/com/interest/service/InterestService.java
+++ src/main/java/com/interest/service/InterestService.java
@@ -1,12 +1,14 @@
 package com.interest.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.interest.config.Response;
 import com.interest.pojo.Interest;
+import java.util.Map;
 
 /**
  * @author a1002
  */
-public interface InterestService {
+public interface InterestService extends IService<Interest> {
     /**
      * fetch data by rule id
      * @param interest rule id
@@ -27,5 +29,14 @@ public interface InterestService {
      * @return Result<XxixDO>
      */
     Response updateStudent(Interest interest);
+
+    /**
+     * fetch data by rule id
+     * @param pages rule id
+     * @return Result<XxixDO>
+     */
+    Map queryInterestByPages(int pages, int num);
+
+    Map queryInterestVague(String thing,int pages,int num);
 
 }
