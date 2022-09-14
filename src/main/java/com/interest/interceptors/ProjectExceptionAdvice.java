5152e1ff280c94ada98fe36b9f66578da875df2a
郑嘉茜 <2137348622@qq.com>
2022-09-14 17:02:37 +0800

    g

--- src/main/java/com/interest/interceptors/ProjectExceptionAdvice.java
+++ src/main/java/com/interest/interceptors/ProjectExceptionAdvice.java
@@ -1,23 +1,23 @@
-package com.interest.interceptors;
-
-import com.interest.config.Response;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-/**
- * @author a1002
- */
-//作为springmvc的异常处理器
-//@ControllerAdvice
-@RestControllerAdvice
-public class ProjectExceptionAdvice {
-    //拦截所有的异常信息
-    @ExceptionHandler(Exception.class)
-    public Response doException(Exception ex){
-        //记录日志
-        //通知运维
-        //通知开发
-        ex.printStackTrace();
-        return Response.ok("服务器异常，请稍后重试！");
-    }
-}
+//package com.interest.interceptors;
+//
+//import com.interest.config.Response;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+///**
+// * @author a1002
+// */
+////作为springmvc的异常处理器
+////@ControllerAdvice
+//@RestControllerAdvice
+//public class ProjectExceptionAdvice {
+//    //拦截所有的异常信息
+//    @ExceptionHandler(Exception.class)
+//    public Response doException(Exception ex){
+//        //记录日志
+//        //通知运维
+//        //通知开发
+//        ex.printStackTrace();
+//        return Response.ok("服务器异常，请稍后重试！");
+//    }
+//}
