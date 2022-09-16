5aecec83a21fae0c5e840fc3ad89829926613096
杨赛雅 <10715064+yang-saiya@user.noreply.gitee.com>
2022-09-16 21:27:26 +0800

    小西西大笨dan

--- src/main/java/com/interest/controller/InterestController.java
+++ src/main/java/com/interest/controller/InterestController.java
@@ -44,7 +44,7 @@ public class InterestController {
     @PutMapping("/update")
     @ResponseBody
     @CrossOrigin
-    public Response updateStudent(@RequestParam Interest interest) {
+    public Response updateStudent(Interest interest) {
         return interestService.updateStudent(interest);
     }
 
